afe787834a34d279cf5d6c58d778162fd252610c
lavilu124 <127327520+lavilu124@users.noreply.github.com>
2025-01-15 14:09:24 +0200

    cleaned code

--- MicroEngine/src/Micro/SystemLogic/Light/include/LightSystem/LightSystem.cpp
+++ MicroEngine/src/Micro/SystemLogic/Light/include/LightSystem/LightSystem.cpp
@@ -1,25 +1,3 @@
-/*
-Copyright DaiMysha (c) 2015, All rights reserved.
-DaiMysha@gmail.com
-https://github.com/DaiMysha
-
-Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
-
-Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
-
-Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation
-and/or other materials provided with the distribution.
-
-THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
-THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS
-BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
-GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
-LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-
-*/
-
-#include <iostream>
-
 #include <SFML/OpenGL.hpp>
 
 #include "../DMUtils/sfml.hpp"
@@ -46,7 +24,7 @@ namespace ls
 
         if(!_lightAttenuationShader.loadFromMemory(staticdata::LIGHT_ATTENUATION_SHADER,sf::Shader::Fragment))
         {
-            std::cerr << "Missing light attenuation Shader. System won't work" << std::endl;
+            //log("Missing light attenuation Shader. System won't work" << std::endl);
         }
 
         _shadowSystem = new ShadowSystem();
