3ec8c964e67578ed7afe203c92718a850f7c58da
lavilu124 <127327520+lavilu124@users.noreply.github.com>
2025-01-20 23:24:28 +0200

    added new light

---
+++ MicroEngine/src/Micro/SystemLogic/Light/Constants.cpp
@@ -0,0 +1,5 @@
+#include "Constants.hpp"
+
+namespace sfu{
+    const float PI = 3.1415926f;
+}
---
+++ MicroEngine/src/Micro/SystemLogic/Light/Constants.hpp
@@ -0,0 +1,13 @@
+#ifndef __CANDLE_CONSTANTS_HPP__
+#define __CANDLE_CONSTANTS_HPP__
+
+#include "../../Core.h"
+
+namespace sfu{
+    /**
+     * PI constant.
+     */
+    extern const float PI;
+}
+
+#endif
