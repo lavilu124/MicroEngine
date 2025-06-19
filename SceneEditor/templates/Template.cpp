e9cdec9878c8765298f7b38e17f6c21bebe1a46f
lavilu124 <127327520+lavilu124@users.noreply.github.com>
2025-06-19 21:08:29 +0300

    fixed template

--- SceneEditor/templates/Template.cpp
+++ SceneEditor/templates/Template.cpp
@@ -1,5 +1,7 @@
 #include "Template.h"
 
+REGISTER_GAMEOBJECT(Template);
+
 Template::Template (Micro::SystemManager* systemManager, const sf::Sprite& sprite, const std::string& name, Micro::Collision::collisionLayer layer, int level)
     : GameObject(systemManager, sprite, name, layer, level)
 {	
